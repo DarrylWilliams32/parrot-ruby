f62d495dcc0d79e71ba7824f5d699eef5730a5b5
darryl_williams <darrylwilliams6@gmail.com>
2021-02-07 16:15:25 -0500

    commit tree aae61030697e75aff27877c9ea079326fa2f3611

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,24 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+    puts "Squawk!"
+end
+parrot
+
+def parrot(phrase = "Squawk!")
+    puts phrase
+    "Squawk!"
+end
+parrot
+
+def parrot(phrase = "Squawk!")
+    puts phrase
+    end 
+    parrot("Pretty bird!")
+    
+    
+ def parrot(phrase = "Squawk!")
+    puts phrase
+    "Pretty bird!"
+    end 
+    parrot("Pretty bird!")
